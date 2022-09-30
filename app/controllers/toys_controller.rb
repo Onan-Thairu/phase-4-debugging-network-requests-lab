664c2f199bea98245577ee694185b50bbfe04469
OnanThairu <thairuonan@gmail.com>
2022-09-30 16:31:47 +0300

    Done.

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,5 +1,7 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_record_response
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
 
   def index
     toys = Toy.all
@@ -7,17 +9,18 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy = find_toy
+    toy.update!(toy_params)
+    render json: toy
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
@@ -26,6 +29,18 @@ class ToysController < ApplicationController
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def find_toy
+    Toy.find(params[:id])
+  end
+
+  def invalid_record_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def record_not_found_response
+    render json: { errors: "Record Not Found" }
   end
 
 end
